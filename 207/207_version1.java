29dab9c1c68769de431c8b3c3335b9796562ffd8
Runhua Gao <rgao9@ur.rochester.edu>
2019-05-23 21:27:29 +0800

    207:Topological search

---
+++ 207/207_version1.java
@@ -0,0 +1,30 @@
+class Solution {
+    private int[] visited;
+    private List<ArrayList<Integer>> nodes;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(numCourses==0) return true;
+        nodes = new ArrayList<ArrayList<Integer>>();
+        for(int i=0;i<numCourses;i++) nodes.add(new ArrayList<>());
+        visited = new int[numCourses];
+        // establish the graph: add neighbor points
+        for(int[] condition:prerequisites){
+            int curr = condition[0],pre = condition[1];
+            nodes.get(curr).add(pre);
+        }
+        for(int i = 0;i<numCourses;i++){
+            if(dfs(i)==true)return false;
+        }
+        return true;
+    }
+    
+    private boolean dfs(int node){
+        if(visited[node]==1) return true;
+        if(visited[node]==2) return false;
+        visited[node] = 1;
+        for(int i:nodes.get(node)){
+            if(dfs(i)) return false;
+        }
+        visited[node] = 2;
+        return false;
+    }
+}
